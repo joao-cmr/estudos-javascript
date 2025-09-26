0a37773539f96a77abe80c5ede5fc322f169d6d3
joao-cmr <contatojoaocmr@gmail.com>
2025-09-26 18:52:01 -0300

    modo estrito
    declaração obrigatória da variavel

---
+++ aula02.js
@@ -0,0 +1,9 @@
+"use strict" 
+/*
+No modo estrito "USE STRICT" eu preciso declarar a variavel,
+mas sem o modo estrito no exemplo abaixo eu não precisaria declarar nome como 
+uma variavel
+*/ 
+
+let nome = "joão"
+console.log(nome)
