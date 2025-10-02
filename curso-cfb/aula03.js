3b5ba7745e23513a1e59134c76f329bdc20e3400
joao-cmr <contatojoaocmr@gmail.com>
2025-10-01 22:55:43 -0300

    Var, Let , Const

---
+++ curso-cfb/aula03.js
@@ -0,0 +1,27 @@
+"use strict"
+/*
+function teste(){
+    let nome = "Carlos"
+    if(true){
+        console.log("Dentro do If do teste " + nome)
+}
+    console.log("Dentro do teste " + nome)
+}
+
+teste()
+
+console.log("fora do teste: " + nome)
+*/
+
+const curso = "JavaScript"
+
+curso = "HTML"
+
+console.log(curso)
+
+
+
+ let nome = "Carlos"
+ nome = "João"
+ nome=10
+    
