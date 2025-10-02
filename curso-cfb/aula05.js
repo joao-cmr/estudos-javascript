85d68717527e39e94893e0ba0d67e2f9c9542118
joao-cmr <contatojoaocmr@gmail.com>
2025-10-02 09:49:43 -0300

    Operadores relacionais

---
+++ curso-cfb/aula05.js
@@ -0,0 +1,12 @@
+// Operadores relacionais > >= < <= == != (! negação) not
+
+let num1 = 10
+let num2 = 5
+let num3 = 10
+
+console.log(num1 > num2) // true
+console.log(num1 < num2) // false
+console.log(num1 >= num3) // true
+console.log(num1 <= num3) // true
+console.log(num1 == num3) // true
+console.log(!(num1 != num2)) // false
