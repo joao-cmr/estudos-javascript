7600c3d22442cdb55ac146fed67e33e3940ffd69
joao-cmr <contatojoaocmr@gmail.com>
2025-10-10 20:32:09 -0300

    OPERADORES DE INCREMETO E DECREMENTO

---
+++ curso-cfb/aula09.js
@@ -0,0 +1,24 @@
+let n=10
+let x=-10
+
+/*
+++n -> incrementa n e retorna o valor incrementado
+n++ -> retorna o valor de n e depois incrementa n
+--n -> decrementa n e retorna o valor decrementado
+n-- -> retorna o valor de n e depois decrementa n
+n-- -> n = n - 1
+n++ -> n = n + 1
+
+*/
+
+
+// console.log(x + "" + n) concatenar. 
+console.log(++n)
+console.log(n++)
+console.log(n)
+console.log(--n)
+console.log(n--)
+console.log(n)
+console.log(x++)
+console.log(++x)
+console.log(x)
