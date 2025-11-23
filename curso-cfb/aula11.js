defac040174d48c07f24fd1aebfd5a7300e8c29b
joao-cmr <contatojoaocmr@gmail.com>
2025-11-22 21:10:30 -0300

    typeof

---
+++ curso-cfb/aula11.js
@@ -0,0 +1,16 @@
+let v1=10
+let v2="10"
+let v3=v1===v2
+let v4={nome:"Carlos"}
+
+/*
+console.log(v1)
+console.log(v2)
+console.log(v3)
+console.log(v4)
+*/
+
+console.log(typeof v1) //number
+console.log(typeof v2) //string
+console.log(typeof v3) //boolean
+console.log(typeof v4) //object
